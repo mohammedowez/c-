d9aa05f38e7df47254f6226be5785ff4cc297548
Mohammed Owez <59692344+mohammedowez@users.noreply.github.com>
2021-02-23 12:23:10 +0530

    Create Kadane's_algorithm.cpp

---
+++ Kadane's_algorithm.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	
+	int a[n];
+	for(int i=0;i<n;i++)
+		cin>>a[i];
+	
+	int cs=0,os=0;
+	for(int i=0;i<n;i++)
+	{
+		if(cs>=0)
+		{
+			cs=cs+a[i];
+		} 
+		else
+		cs=a[i]; 
+		
+		if(cs>os)
+			os=cs;
+		
+	}  
+	
+	cout<<os<<"\n";
+}
+
+int cs = nums[0];
+   int ns = nums[0];
+ 
+   for (int i = 1; i < nums.size(); i++)
+   {
+        cs = max(nums[i], cs+nums[i]);
+        ns = max(ns, cs);
+   }
+   return ns;
+    } 
