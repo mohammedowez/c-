c1cc1cc8b4b648efcab5149a544ed664e123308c
Mohammed Owez <59692344+mohammedowez@users.noreply.github.com>
2021-04-11 08:29:08 +0530

    Update Nearest_smallest_right.cpp

--- Ds/Stack/Nearest_smallest_right.cpp
+++ Ds/Stack/Nearest_smallest_right.cpp
@@ -1,46 +1,23 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-
-
-
-vector<long long> getNSL(long long arr[], int n)
-{
-    vector<long long> nsl;
-    stack<pair<long long, int>> myStack;
-    
-    for(int i=0; i<n; i++){
-        while(!myStack.empty() and myStack.top().first >= arr[i])
-		{
-            myStack.pop();
-        }
-        if(myStack.empty())
-		{
-            nsl.push_back(-1);
-        }
-        else
-		{
-            nsl.push_back(myStack.top().second);
-        }
-        myStack.push({arr[i], i});
-    }
-    
-    return nsl;
-}
 
 vector<long long> getNSR(long long arr[], int n)
 {
     vector<long long> nsr;
     stack<pair<long long, int>> myStack;
     
-    for(int i=n-1; i>=0; i--){
-        while(!myStack.empty() and myStack.top().first >= arr[i]){
+    for(int i=n-1; i>=0; i--)
+	{
+        while(!myStack.empty() and myStack.top().first >= arr[i])
+		{
             myStack.pop();
         }
-        if(myStack.empty()){
+        if(myStack.empty())
+		{
             nsr.push_back(n);
         }
-        else{
+        else
+		{
             nsr.push_back(myStack.top().second);
         }
         myStack.push({arr[i], i});
@@ -49,52 +26,21 @@ vector<long long> getNSR(long long arr[], int n)
     return nsr;
 }
 
-class Solution
-{
-    public:
-    long long getMaxArea(long long arr[], int n){
-        vector<long long> nsl = getNSL(arr, n);
-        vector<long long> nsr = getNSR(arr, n);
-        vector<long long> widths(n);
-        
-        for(int i=0; i<n; i++){
-            widths[i] = nsr[i] - nsl[i] - 1;
-        }
-        
-        long long maxArea = widths[0] * arr[0];
-        for(int i=1; i<n; i++){
-            long long area = widths[i] * arr[i];
-            if(area > maxArea){
-                maxArea = area;
-            }
-        }
-        return maxArea;
-    }
-};
-
-
-// { Driver Code Starts.
-
 int main()
- {
-    long long t;
+{
+	int n;
+	cin>>n;
 
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-        
-        long long arr[n];
-        for(int i=0;i<n;i++)
-            cin>>arr[i];
-        Solution ob;
-        cout<<ob.getMaxArea(arr, n)<<endl;
-    
-    }
-	return 0;
+	long long a[n];
+	for(int i=0;i<n;i++)
+		cin>>a[i];
+		
+	vector<long long > m=getNSR(a,n);
+	for(auto i : m)
+		cout<<i<<" ";	
+	
+	return 0;	
 }
-  // } Driver Code Ends
 
 ip=  6 2 5 4 5 1 5
 op=  1 5 3 5 5 7 7 
