b7253ef45f8cb11cc28e08e8fb512dc80525b3e8
Mohammed Owez <59692344+mohammedowez@users.noreply.github.com>
2021-04-19 16:52:18 +0530

    Create printing   LCS.cpp

---
+++ Ds/Dynamic programming/LCS/printing   LCS.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution
+{
+    public:
+    //Function to find the length of longest common subsequence in two strings.
+    string longestCommonSubstr(int x, int y, string s1, string s2)
+    {
+        // your cod[y+e here
+        
+        
+        int dp[x+1][y+1];
+        
+        memset(dp,0,sizeof(dp));
+        
+        for(int i=1;i<=x;i++)
+        {
+            for(int j=1;j<=y;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                } 
+                else
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        string s="";
+        
+        int i=x,j=y;
+        while(i>0 &&  j>0)
+        {
+        	if(s1[i-1]==s2[j-1])
+        	{
+        		s.push_back(s1[i-1]);
+        		i--;
+				j--;
+			}
+			else
+			{
+				if(dp[i-1][j] > dp[i][j-1])
+				{
+					i--;
+				}
+				else
+				j--;
+			 } 
+		} 
+		reverse(s.begin(),s.end());
+		return s;
+    } 
+    
+};
+
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n, m; cin >> n >> m;
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr ( n, m,s1,s2) << endl;
+    }
+}
