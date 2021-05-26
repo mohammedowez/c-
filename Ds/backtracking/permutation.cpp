2d5520af394f56dcd2f5ae1e52d31f82f6ade538
Mohammed Owez <59692344+mohammedowez@users.noreply.github.com>
2021-05-26 19:24:07 +0530

    Update permutation.cpp

--- Ds/backtracking/permutation.cpp
+++ Ds/backtracking/permutation.cpp
@@ -1,1 +1,27 @@
-
+class Solution
+{
+	public:
+	vector<string> v;
+	void solve(string s,int i)
+	{
+	    if(s[i]=='\0')
+	    {
+	        v.push_back(s);
+	        return;
+	    } 
+	    for(int j=i;s[j]!='\0';j++)
+	    {
+	        swap(s[i],s[j]);
+	        solve(s,i+1);
+	        swap(s[i],s[j]);
+	    }
+	}
+		vector<string>find_permutation(string S)
+		{
+		    // Code here there
+		    sort(S.begin(),S.end());
+		    solve(S,0); 
+		    sort(v.begin(),v.end());
+		    return v;
+		}
+};
