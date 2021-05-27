9e6a35da92994dd48ff07fa7307b953e92fc16a7
Mohammed Owez <59692344+mohammedowez@users.noreply.github.com>
2021-05-27 14:58:12 +0530

    Update Possible Words From Phone Digits .cpp

--- Ds/backtracking/Possible Words From Phone Digits .cpp
+++ Ds/backtracking/Possible Words From Phone Digits .cpp
@@ -33,3 +33,53 @@ public:
         
     }
 };
+
+
+
+#include<bits/stdc++.h>
+#define ll long long int
+# define pb push_back  
+using namespace std;
+
+
+vector<string> mappings = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"}, ans; 
+
+void helper(string &digits, int i, string combi)
+{	
+	if(i == size(digits)) 
+	{  // base condition - push when we have recursed for all digits
+		ans.push_back(combi);    
+		return;
+	}
+	// add and recurse for each character (chosen from current digit-charcter mapping)
+	for(auto &c : mappings[digits[i] - '2'])
+	{
+	//	cout<<combi + c<<" ";
+	//	combi.push_back(c);
+		helper(digits, i + 1, combi+c);
+	//	combi.pop_back();
+	}
+}   
+vector<string> letterCombinations(string digits) 
+{
+	if(digits == "") return ans;         // no combinations formed without any digits
+	helper(digits, 0, "");              
+	return ans;
+}
+// recursive helper function
+
+
+
+int main()
+{
+	string s;
+	cin>>s;
+	letterCombinations(s);
+	cout<<"\n"<<" "<<"printing answer   ";
+	for(auto i:ans)
+	{
+		cout<<i<<" ";
+	} 
+	
+	return 0;
+}
