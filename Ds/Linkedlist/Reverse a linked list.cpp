5cd74fe6e12f1477cb7518c7346930e2ce8a51c8
Mohammed Owez <59692344+mohammedowez@users.noreply.github.com>
2021-05-29 10:24:50 +0530

    Create Reverse a linked list.cpp

---
+++ Ds/Linkedlist/Reverse a linked list.cpp
@@ -0,0 +1,83 @@
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+
+
+class Solution
+{
+    public:
+    //Function to reverse a linked list.
+     Node* reverseList( Node *head)
+    {
+        // code here
+        // return head of reversed list
+         Node *curr,*prev,*next;
+        curr=head;
+        prev=NULL;
+        while(curr!=NULL)
+        {
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+};
+    
+
+
+// { Driver Code Starts.
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+       printf("%d ", temp->data);
+       temp  = temp->next;
+    }
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n,l,firstdata;
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        
+        for (int i=1; i<n; i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+        
+        Solution ob;
+        head = ob. reverseList(head);
+        
+        printList(head);
+        cout << endl;
+    }
+    return 0;
+}
+
+ 
